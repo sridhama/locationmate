bc163eb9e3e82913f87c5960620d6dcbc31732cf
Sridhama Prakhya <sridhama@sridhama.com>
2017-04-06 11:16:36 +0530

    updates

--- delete_account.php
+++ delete_account.php
@@ -1,10 +1,10 @@
 <?php
-require("./configure.php");
+require("./config.php");
 $phone = $_GET['phone'];
-$sql = "DELETE * FROM `users` WHERE `phone` = '$phone'";
+$sql = "DELETE FROM `users` WHERE `phone` = '$phone'";
 $rs = mysqli_query($GLOBALS['connection'],$sql);
-$sql = "DELETE * FROM `friends_meta` WHERE `phone` = '$phone';";
+$sql = "DELETE FROM `friends_meta` WHERE `phone` = '$phone';";
 $rs = mysqli_query($GLOBALS['connection'],$sql);
-$sql = "DELETE * FROM `friends_meta` WHERE `friend_phone` = '$phone';";
+$sql = "DELETE FROM `friends_meta` WHERE `friend_phone` = '$phone';";
 $rs = mysqli_query($GLOBALS['connection'],$sql);
 echo "SUCCESS";
